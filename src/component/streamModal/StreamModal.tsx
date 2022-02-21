441f216721b9ded249b28f5909782034c8e1a71e
Mendes <flaviojmendes@gmail.com>
2022-02-21 00:38:01 +0000

    adding preview modal

---
+++ src/component/streamModal/StreamModal.tsx
@@ -0,0 +1,126 @@
+import React from "react";
+import {
+  Button,
+  Center,
+  chakra,
+  Flex,
+  Grid,
+  GridItem,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Tag,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { StreamerModel } from "../../model/StreamerModel";
+import "./StreamModal.css";
+
+interface Props {
+  streamer: StreamerModel;
+  timeStreaming: string;
+}
+
+export default function StreamModal(props: Props) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button
+        size="xs"
+        float="right"
+        onClick={onOpen}
+        _hover={{
+          background: "primary.700",
+          color: "white",
+        }}
+        backgroundColor="primary.800"
+        color="white"
+      >
+        Prévia
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent className="modal-content">
+          <ModalHeader p="4">
+            <Flex>
+              <Image
+                w="10"
+                mr="4"
+                borderRadius="full"
+                src={props.streamer.profile_image_url}
+                alt={props.streamer.user_name}
+              />
+              <chakra.span mt='1'>{props.streamer.user_name}</chakra.span>
+              <Tag
+                size="sm"
+                ml="5"
+                mt="auto"
+                mb="auto"
+                variant="solid"
+                bg="red.500"
+              >
+                {props.timeStreaming}
+              </Tag>
+            </Flex>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody p="1">
+            <Flex h="100%">
+              <Center className="modal-stream">
+                <iframe
+                  title={props.streamer.user_login}
+                  src={
+                    "https://player.twitch.tv/?channel=" +
+                    props.streamer.user_name +
+                    "&parent=" +
+                    process.env.REACT_APP_DOMAIN +
+                    "&enableExtensions=false&muted=false&volume=1.0&quality=auto&controls=true&allowFullScreen=true"
+                  }
+                  height="100%"
+                  width="100%"
+                  onClick={() => console.log("clicou ")}
+                ></iframe>{" "}
+              </Center>
+              <Center className="modal-chat">
+                <iframe
+                  title={props.streamer.user_login}
+                  src={
+                    "https://www.twitch.tv/embed/" +
+                    props.streamer.user_name +
+                    "/chat?parent=" +
+                    process.env.REACT_APP_DOMAIN
+                  }
+                  height="100%"
+                  width="100%"
+                  onClick={() => console.log("clicou ")}
+                ></iframe>
+              </Center>
+            </Flex>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              float="right"
+              _hover={{
+                background: "primary.700",
+                color: "white",
+              }}
+              backgroundColor="primary.800"
+              color="white"
+              mr={0}
+              onClick={onClose}
+            >
+              Fechar
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
--- src/component/streamerCard/StreamerCard.tsx
+++ src/component/streamerCard/StreamerCard.tsx
@@ -7,6 +7,8 @@ import {
   Box,
   Divider,
   Flex,
+  Grid,
+  GridItem,
   Image,
   Link,
   Tag,
@@ -16,6 +18,7 @@ import React from "react";
 import { chakra } from "@chakra-ui/react";
 import { StreamerModel } from "../../model/StreamerModel";
 import axios from "axios";
+import StreamModal from "../streamModal/StreamModal";
 import FingerprintJS from "@fingerprintjs/fingerprintjs";
 var format = require("format-duration");
 const fpPromise = FingerprintJS.load();
@@ -57,13 +60,20 @@ export default function StreamerCard(props: Props) {
   const showPreview = (user_name: string) => {
     return (
       <>
-        <chakra.div position="absolute" height="180px" width="100%" backgroundColor="rgba(255,255,255,0)"></chakra.div>
+        <chakra.div
+          position="absolute"
+          height="180px"
+          width="100%"
+          backgroundColor="rgba(255,255,255,0)"
+        ></chakra.div>
         <iframe
           title={streamer.user_login}
           src={
             "https://player.twitch.tv/?channel=" +
             streamer.user_name +
-            "&parent=" + process.env.REACT_APP_DOMAIN + "&enableExtensions=false&muted=true&quality=low&controls=false"
+            "&parent=" +
+            process.env.REACT_APP_DOMAIN +
+            "&enableExtensions=false&muted=true&quality=low&controls=false"
           }
           height="180"
           width="100%"
@@ -131,21 +141,28 @@ export default function StreamerCard(props: Props) {
       </Link>
 
       <Box pl="5" pr="5" pb="2" pt="4">
-        <Link
-          href={"https://twitch.tv/" + streamer.user_name}
-          isExternal={true}
-          onClick={() => logClick(streamer.user_login)}
-        >
-          <Flex mt="0" fontWeight="semibold" lineHeight="tight">
-            <Image
-              w="8"
-              borderRadius="full"
-              src={streamer.profile_image_url}
-              alt={streamer.user_name}
-            />
-            &nbsp;{streamer.user_name}
-          </Flex>
-        </Link>
+        <Grid templateColumns="repeat(5, 1fr)" h="100%" gap={6}>
+          <GridItem w="100%" colSpan={3}>
+            <Link
+              href={"https://twitch.tv/" + streamer.user_name}
+              isExternal={true}
+              onClick={() => logClick(streamer.user_login)}
+            >
+              <Flex fontWeight='semibold'>
+                <Image
+                  w="8"
+                  borderRadius="full"
+                  src={streamer.profile_image_url}
+                  alt={streamer.user_name}
+                />
+                &nbsp;{streamer.user_name}
+              </Flex>
+            </Link>
+          </GridItem>
+          <GridItem w="100%" colSpan={2}>
+            <StreamModal streamer={streamer} timeStreaming={timeStreaming}></StreamModal>
+          </GridItem>
+        </Grid>
         <chakra.p mt="2" fontWeight="semibold" lineHeight="tight" isTruncated>
           <Tooltip label={streamer.title} aria-label="A tooltip">
             {streamer.title}
