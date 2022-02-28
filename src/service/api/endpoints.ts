0eb4ea1c5e1c9eaa50288d2fdf7dc65909d23f73
Bruno Bandeira <brunoban.dev@gmail.com>
2022-02-28 21:30:59 +0000

    split axios and endpoints for a different files

---
+++ src/service/api/endpoints.ts
@@ -0,0 +1,11 @@
+export const endpoints = {
+  base: {
+    url: process.env.REACT_APP_API_URL || "http://localhost:8080",
+  },
+  streams: {
+    url: "streams",
+  },
+  vods: {
+    url: "vods",
+  },
+};
