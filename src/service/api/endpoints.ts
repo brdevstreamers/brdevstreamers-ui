8bda5c7305474a4f75b051f94ba830e499afabf2
Mendes <flaviojmendes@gmail.com>
2022-03-01 12:27:48 +0000

    adding gzip compression on nginx

--- src/service/api/endpoints.ts
+++ src/service/api/endpoints.ts
@@ -1,6 +1,6 @@
 export const endpoints = {
   base: {
-    url: process.env.REACT_APP_API_URL || "http://localhost:8080",
+    url: process.env.REACT_APP_API_URL || "http://localhost:8000",
   },
   streams: {
     url: "streams",
