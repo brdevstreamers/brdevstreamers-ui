10ce3844ac25348722cdffbf6ee26f05b69a7f2a
Mendes <flaviojmendes@gmail.com>
2022-03-03 00:33:23 +0000

    raids prototype

--- src/component/raids/Raids.tsx
+++ src/component/raids/Raids.tsx
@@ -1,23 +1,103 @@
 import { Heading } from "@chakra-ui/react";
-import CytoscapeComponent from 'react-cytoscapejs';
-
-
-const elements = [
-    { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-    { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-    { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
- ];
+import { useState } from "react";
+import {
+  Canvas,
+  Node,
+  EdgeData,
+  NodeData,
+  createEdgeFromNodes,
+  hasLink,
+} from "reaflow";
 
 
 export default function Raids() {
+    const [nodes, setNodes] = useState<NodeData[]>([
+        {
+          id: "1",
+          text: "marcobruno",
+        },
+        {
+          id: "2",
+          text: "flaviojmendes",
+        },
+        {
+          id: "3",
+          text: "nandosangenetto",
+        },
+        {
+          id: "4",
+          text: "flaviojmendes",
+        },
+        {
+          id: "5",
+          text: "sofiarochinha",
+        },
+        {
+          id: "6",
+          text: "pvr1z",
+        }
+      ]);
+      
+      const [edges, setEdges] = useState<EdgeData[]>([
+        {
+          id: "1-2",
+          from: "1",
+          to: "2",
+        },
+        {
+          id: "2-3",
+          from: "2",
+          to: "3",
+        },
+        {
+          id: "3-4",
+          from: "3",
+          to: "4",
+        },
+        {
+          id: "5-4",
+          from: "5",
+          to: "4",
+        },
+        {
+          id: "6-5",
+          from: "6",
+          to: "5",
+        },
+        
+      ]);
+      
   return (
     <>
       <Heading as="h3" size="sm" mb="20px">
         Minhas Raids
       </Heading>
 
-       <CytoscapeComponent elements={elements} style={ { width: '500px', height: '500px' } } />
-      
+      <div>
+        <Canvas
+          nodes={nodes}
+          edges={edges}
+          maxWidth={1000}
+          maxHeight={700}
+          node={<Node dragType="node" />}
+          onNodeLinkCheck={(_event, from: NodeData, to: NodeData) => {
+            if (from.id === to.id) {
+              return false;
+            }
+
+            if (hasLink(edges, from, to)) {
+              return false;
+            }
+
+            return true;
+          }}
+          onNodeLink={(_event, from, to) => {
+            const newEdges = edges.filter((e) => e.to !== from.id);
+
+            setEdges([...newEdges, createEdgeFromNodes(to, from)]);
+          }}
+        />
+      </div>
     </>
   );
 }
