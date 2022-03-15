1a3624645690fa7fa183e9ddf682adf377f3912e
Bruno Bandeira <brunoban.dev@gmail.com>
2022-03-15 14:48:08 +0000

    creating file to save extended theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,31 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/react";
 import ReactGA from "react-ga";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter } from "react-router-dom";
-
-// 2. Call `extendTheme` and pass your custom values
-const theme = extendTheme({
-  colors: {
-    primary: {
-      400: "#D3C8FF",
-      500: "#C299FF",
-      600: "#B685FF",
-      700: "#A970FF",
-      800: "#9D5CFF",
-      900: "#6200F5",
-    },
-    secondary: {
-      400: "#ADAFB8",
-      500: "#858794",
-      600: "#33374D",
-      700: "#1F2A3D",
-      800: "#0E1724",
-    },
-  },
-});
+import theme from "./theme";
 
 ReactGA.initialize(process.env.REACT_APP_GA_ID || "");
 
