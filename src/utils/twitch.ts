300ba1c30e9dd3001f30079f7604129828075907
Carlos Silva <carlosqsilva@outlook.com>
2022-03-19 19:02:47 -0300

    feat: add chat messages to Mosaic modal

--- src/utils/twitch.ts
+++ src/utils/twitch.ts
@@ -1,9 +1,11 @@
+const isDevelopment = process.env.NODE_ENV !== "production";
+
 export const getEmbedUrl = (channel: string): string => {
   return (
     "https://player.twitch.tv/?channel=" +
     channel +
     "&parent=" +
-    process.env.REACT_APP_DOMAIN +
+    (isDevelopment ? "localhost" : process.env.REACT_APP_DOMAIN) +
     "&enableExtensions=false&muted=false&volume=1.0&quality=auto&controls=true&allowFullScreen=true"
   );
 };
