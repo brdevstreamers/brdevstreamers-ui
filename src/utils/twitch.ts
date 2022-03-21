42cb40de3b6f5dd9c45ac00c07417e8d5973a9ca
Carlos Silva <carlosqsilva@outlook.com>
2022-03-21 14:35:16 -0300

    chore: refactor function

--- src/utils/twitch.ts
+++ src/utils/twitch.ts
@@ -1,11 +1,10 @@
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 export const getEmbedUrl = (channel: string): string => {
+  const domain = isDevelopment ? "localhost" : process.env.REACT_APP_DOMAIN;
+
   return (
-    "https://player.twitch.tv/?channel=" +
-    channel +
-    "&parent=" +
-    (isDevelopment ? "localhost" : process.env.REACT_APP_DOMAIN) +
+    `https://player.twitch.tv/?channel=${channel}&parent=${domain}` +
     "&enableExtensions=false&muted=false&volume=1.0&quality=auto&controls=true&allowFullScreen=true"
   );
 };
